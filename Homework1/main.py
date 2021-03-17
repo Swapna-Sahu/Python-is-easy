ae60523e59c52acc2f4da7be5f32d300e81baf7b
Swapna <swapna.sahu85@gmail.com>
2021-03-17 23:14:17 +0100

    homework#1

---
+++ Homework1/main.py
@@ -0,0 +1,34 @@
+# Album Details
+
+"""
+Album detail data assigned
+to variable
+"""
+
+Album = "This is me"
+Artist = "Sana Singh"
+Singer = "Alisha Sharma"
+Writer = "Vinod Khanna"
+Musician = "Kishore Kapoor"
+Released = "12/12/2020"
+Genre = "Jazz"
+Duration= 6.37
+Recorded = "11/11/2020"
+Producer = "Saimra Khan"
+Rating = 4
+
+"""
+Print the variable
+to see if it showing correct data
+"""
+print(Album)
+print(Artist)
+print(Singer)
+print(Writer)
+print(Musician)
+print(Released)
+print(Genre)
+print(Duration)
+print(Recorded)
+print(Producer)
+print(Rating)
